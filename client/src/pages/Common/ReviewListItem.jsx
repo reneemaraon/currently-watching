4d2dbccca8a5dce7ec17bfa7e254e51d869fc58c
Francesca Renee Maraon <reneemaraon@gmail.com>
2024-02-12 22:59:55 +0800

    on hover pointer setting

--- client/src/pages/Common/ReviewListItem.jsx
+++ client/src/pages/Common/ReviewListItem.jsx
@@ -27,7 +27,7 @@ const ReviewsListItem = ({ id }) => {
 
     return (
         <div onClick={onClickNavigate} className="
-            ReviewListItem overflow-hidden max-[600px]:flex-col max-[600px]:h-[400px] min-w-[550px] h-[260px] bg-theme-base bg-opacity-70 rounded-[20px] border border-slate-200 justify-start items-start inline-flex
+            ReviewListItem cursor-pointer overflow-hidden max-[600px]:flex-col max-[600px]:h-[400px] min-w-[550px] h-[260px] bg-theme-base bg-opacity-70 rounded-[20px] border border-slate-200 justify-start items-start inline-flex
             max-[600px]:min-w-[320px]  max-[600px]:min-h-[450px] 
         ">
             <div className="
