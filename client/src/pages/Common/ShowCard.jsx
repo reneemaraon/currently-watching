c4c24e40bce1f9e794d20f26ad0c6e9955f035e0
Francesca Renee Maraon <reneemaraon@gmail.com>
2024-02-12 22:59:31 +0800

    tabs corrected and added navigate to show detail

--- client/src/pages/Common/ShowCard.jsx
+++ client/src/pages/Common/ShowCard.jsx
@@ -1,35 +1,41 @@
 import StarIcon from "./Star";
+import { useNavigate } from "react-router-dom"
 
-const ShowCardSmall = () => {
-    return (
-        <div className="overflow-hidden w-64 shrink-0 h-[214px] bg-theme-base bg-opacity-70 rounded-[17px] flex-col justify-start items-start inline-flex">
-            <img className="Show h-[135px]" src="https://via.placeholder.com/262x135" />
-            <div className="ShowDetails w-full px-[13px] py-[11px] justify-between items-start inline-flex">
-                <div className="ShowDetailsText w-40 h-[50px] pl-[5px] pt-[5px] flex-col justify-between items-start inline-flex">
-                    <div className="inline-flex gap-1">
-                        <span className="text-sm font-bold">
-                            The Glory 
-                        </span>
-                        <span className="text-sm font-normal">
-                            (2022)
-                        </span>
-                    
-                    </div>
-                <div className="NoOfReviews text-slate-500 text-xs font-medium">
-                    12333 reviews
-                </div>
-            </div>
-            <div className="StarRating w-14 h-14 rounded-full border-2 border-brand-tq justify-center items-center gap-0.5 flex">
-                <div className="text-[15px] font-bold leading-[10.50px]">
-                    4.8
-                </div>
-                <div className="w-4 h-4">
-                    <StarIcon />
-                </div>
-            </div>
-            </div>
+const ShowCardSmall = ({ id }) => {
+  const navigate = useNavigate();
+
+  const onClickNavigate = () => {
+    navigate(`/shows/${id || "1"}`);
+  }
+  
+  return (
+    <div onClick={onClickNavigate} className="cursor-pointer overflow-hidden w-64 shrink-0 h-[214px] bg-theme-base bg-opacity-70 rounded-[17px] flex-col justify-start items-start inline-flex">
+      <img className="Show h-[135px]" src="https://via.placeholder.com/262x135" />
+      <div className="ShowDetails w-full px-[13px] py-[11px] justify-between items-start inline-flex">
+        <div className="ShowDetailsText w-40 h-[50px] pl-[5px] pt-[5px] flex-col justify-between items-start inline-flex">
+          <div className="inline-flex gap-1">
+            <span className="text-sm font-bold">
+                The Glory 
+            </span>
+            <span className="text-sm font-normal">
+                (2022)
+            </span>
+          </div>
+        <div className="NoOfReviews text-slate-500 text-xs font-medium">
+            12333 reviews
         </div>
-    );
+      </div>
+      <div className="StarRating w-14 h-14 rounded-full border-2 border-brand-tq justify-center items-center gap-0.5 flex">
+          <div className="text-[15px] font-bold leading-[10.50px]">
+              4.8
+          </div>
+          <div className="w-4 h-4">
+              <StarIcon />
+          </div>
+      </div>
+      </div>
+    </div>
+  );
 }
  
 export default ShowCardSmall;
