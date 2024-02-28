b14215c0651ef93a8c965a06b07639a21e035943
Francesca Renee Maraon <reneemaraon@gmail.com>
2024-02-28 08:53:56 +0800

    list queries and resolvers graphql setup

--- api/models/user.js
+++ api/models/user.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema ({
-    name: String,
-    screenName: {type: String, default: ''},
-    twitterId: String,
-    profilePhotoUrl: String,
-    role: { type: String, enum: ['user', 'admin'], default: 'user' },
-})
+const userSchema = new Schema({
+  name: { type: String, required: true },
+  screenName: { type: String, default: '' },
+  twitterId: String,
+  profilePhotoUrl: String,
+  role: { type: String, enum: ['user', 'admin'], default: 'user' },
+});
 
 const User = mongoose.model('user', userSchema);
 
--- api/resolvers.js
+++ api/resolvers.js
@@ -6,69 +6,20 @@ const Review = require('./models/review');
 
 const resolvers = {
   Query: {
-    getUserById: async (_, { userId }) => {
-      try {
-        const user = await User.findById(userId);
-        return user;
-      } catch (error) {
-        throw new Error('Failed to fetch user');
-      }
+    users: async () => {
+      let users = await User.find({});
+      return users;
     },
-    getAllShows: async () => {
-      try {
-        const shows = await Show.find();
-        return shows;
-      } catch (error) {
-        throw new Error('Failed to fetch shows');
-      }
+    shows: async () => {
+      let shows = await Show.find({});
+      return shows;
     },
-    getShowById: async (_, { showId }) => {
-      try {
-        const show = await Show.findById(showId);
-        return show;
-      } catch (error) {
-        throw new Error('Failed to fetch show');
-      }
-    },
-    getReviewsByUserId: async (_, { userId }) => {
-      try {
-        const reviews = await Review.find({ user: userId });
-        return reviews;
-      } catch (error) {
-        throw new Error('Failed to fetch reviews');
-      }
+    reviews: async () => {
+      let reviews = await Review.find({});
+      return reviews;
     },
   },
-  Mutation: {
-    createReview: async (
-      _,
-      {
-        userId,
-        showId,
-        overallRating,
-        performanceRating,
-        narrativeRating,
-        cinematographyRating,
-        body,
-      }
-    ) => {
-      try {
-        const review = new Review({
-          user: userId,
-          show: showId,
-          overallRating,
-          performanceRating,
-          narrativeRating,
-          cinematographyRating,
-          body,
-        });
-        await review.save();
-        return review;
-      } catch (error) {
-        throw new Error('Failed to create review');
-      }
-    },
-  },
+  Mutation: {},
 };
 
 module.exports = resolvers;
