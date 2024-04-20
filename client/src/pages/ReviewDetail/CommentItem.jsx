5e9bc6ee7588b26c638e2f18cde83b40f34fe85b
Francesca Renee Maraon <reneemaraon@gmail.com>
2024-04-20 19:49:42 +0800

    comment profile to user link

--- client/src/pages/ReviewDetail/CommentItem.jsx
+++ client/src/pages/ReviewDetail/CommentItem.jsx
@@ -1,14 +1,16 @@
-import formatDateTime from "../../utils/formatDate";
-import Icon from "../Common/Icon";
-import { OptionsIcon } from "../Common/IconList";
-import Dropdown, { Option } from "../Common/Dropdown";
-import { useState, useRef, useEffect } from "react";
-import { useAuthContext } from "../../context/AuthContext";
+import formatDateTime from '../../utils/formatDate';
+import Icon from '../Common/Icon';
+import { OptionsIcon } from '../Common/IconList';
+import Dropdown, { Option } from '../Common/Dropdown';
+import { useState, useRef, useEffect } from 'react';
+import { useAuthContext } from '../../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
 
 const Comment = ({ comment, onDelete }) => {
+  const navigate = useNavigate();
   const [isDropdownVisible, setDropdownVisible] = useState(false);
   const {
-    user: { screenName, name, profilePhotoUrl },
+    user: { _id: userId, screenName, name, profilePhotoUrl },
     commentBody,
     createdAt,
   } = comment;
@@ -25,25 +27,37 @@ const Comment = ({ comment, onDelete }) => {
     }
   };
 
+  const onClickUser = (event) => {
+    navigate(`/users/${userId}`);
+  };
+
   useEffect(() => {
-    document.addEventListener("mousedown", handleOutsideClick);
+    document.addEventListener('mousedown', handleOutsideClick);
     return () => {
-      document.removeEventListener("mousedown", handleOutsideClick);
+      document.removeEventListener('mousedown', handleOutsideClick);
     };
   }, []);
 
   return (
-    <div className="w-full p-4 bg-theme-base rounded-2xl border border-light-stroke flex-col justify-center items-start gap-4 flex">
+    <div className="w-full p-4 hover:bg-main-bg hover:bg-opacity-20 bg-theme-base rounded-2xl border border-light-stroke flex-col justify-center items-start gap-4 flex">
       <div className="CommentHead w-full justify-between items-start inline-flex">
-        <div className="Comment grow shrink basis-0 justify-start items-center gap-3 sm:gap-4 flex">
+        <div className=" grow shrink basis-0 justify-start items-center gap-3 sm:gap-4 flex">
           <img
-            className="ProfilePhoto w-7 h-7 sm:w-8 sm:h-8 rounded-full"
+            onClick={onClickUser}
+            className="ProfilePhoto hover:cursor-pointer hover:opacity-70 w-7 h-7 sm:w-8 sm:h-8 rounded-full"
             src={profilePhotoUrl}
           />
           <div className="Details grow shrink basis-0 flex-col justify-start items-start gap-0.5 inline-flex">
-            <div className="NameUsername w-full justify-start items-center gap-1.5 inline-flex">
-              <div className="author-name">{screenName || "Hwang Inyoup"}</div>
-              <div className="Username info-text">@{name || "hi_high_hiy"}</div>
+            <div
+              onClick={onClickUser}
+              className="group w-full justify-start items-center gap-1.5 inline-flex"
+            >
+              <div className="hover:cursor-pointer group-hover:text-brand-dark-purple author-name">
+                {name}
+              </div>
+              <div className="hover:cursor-pointer Username group-hover:text-brand-dark-purple info-text">
+                @{screenName}
+              </div>
             </div>
             <div className="DatePublished info-text">
               {formatDateTime(createdAt)}
@@ -74,7 +88,7 @@ const Comment = ({ comment, onDelete }) => {
         )}
       </div>
       <p className="CommentText w-full text-sm leading-6 font-normal">
-        {commentBody || ""}
+        {commentBody || ''}
       </p>
     </div>
   );
