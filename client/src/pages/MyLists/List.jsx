12b45db7361c93f19ac3d140779489ef0e63497a
Francesca Renee Maraon <reneemaraon@gmail.com>
2024-05-15 16:48:11 +0800

    minor ui fix

--- client/src/pages/MyLists/List.jsx
+++ client/src/pages/MyLists/List.jsx
@@ -182,8 +182,8 @@ const List = ({ list, index }) => {
   };
 
   return (
-    <div className="List self-stretch flex-col justify-start items-start gap-2 flex">
-      <div className="ListHeader w-full px-1 md:px-2 sm:px-4 py-1 sm:py-2.5 justify-between items-start gap-2.5 inline-flex">
+    <div className="List self-stretch flex-col justify-start items-start gap-2.5 flex">
+      <div className="ListHeader w-full px-1 md:px-2 sm:px-4 py-1 sm:py-2.5 justify-between items-end gap-2.5 inline-flex">
         <input
           type="text"
           className="bg-transparent focus:outline-none grow title-text font-light"
@@ -193,77 +193,79 @@ const List = ({ list, index }) => {
         />
         <ListActions addDrama={addDrama} list={list} />
       </div>
-      <div
-        ref={listRef}
-        className="List transition-all self-stretch px-0 sm:px-1 md:px-2.5 flex-col justify-start items-start gap-1 sm:gap-1.5 flex"
-      >
-        {showItems &&
-          items &&
-          items.map((item, index) => (
-            <Draggable
-              key={item.order}
-              onStop={(e, data) => {
-                const newPosition = index + Math.round(data.y / itemHeight); // Assuming each item's height is 50px
-                handleStop(index, newPosition);
-              }}
-              onStart={() => onDragStart(index)}
-              onDrag={(e, data) => {
-                const newPosition = index + Math.round(data.y / itemHeight); // Assuming each item's height is 50px
-                setInsertIndex(newPosition);
-              }}
-              axis="y"
-              position={{ x: 0, y: item.order - 1 }}
-              // grid={[1, 70]}
-            >
-              <ListItem
-                insertVisible={insertIndex == index && !item.selected}
-                fromTop={draggedIndex < index}
-                order={index + 1}
-                searchRef={searchInputRef}
-                onDelete={onDelete}
-                dragging={dragging}
-                selected={item.selected}
-                show={item.show}
-                setShowToItem={setShowToItem}
-              />
-            </Draggable>
-          ))}
-      </div>
-      {list.items.length > 0 && (
+      <div className="w-full inline-flex flex-col gap-1.5">
         <div
-          className="cursor-pointer self-stretch px-0 sm:px-1 md:px-2.5 flex-col justify-start items-start gap-1 sm:gap-1.5 flex"
-          onClick={toggleShowItems}
+          ref={listRef}
+          className="List transition-all self-stretch px-0 sm:px-1 md:px-2.5 flex-col justify-start items-start gap-1 sm:gap-1.5 flex"
         >
-          {showItems ? (
-            <div className="ShowDetailCard w-full h-5 sm:h-7 bg-theme-base bg-opacity-70 rounded-b-xl rounded-t-lg border border-slate-200 justify-start items-center inline-flex">
-              <div className="Expand grow shrink basis-0 self-stretch justify-center items-center flex">
-                <Icon
-                  sizeRules="w-3 h-3 sm:w-4 sm:h-4"
-                  fill="rotate-180 stroke-2 stroke-brand-tq fill-none"
-                >
-                  <ExpandDownIcon />
-                </Icon>
-              </div>
-            </div>
-          ) : (
-            <div className="ShowDetailCard w-full h-9 bg-white bg-opacity-20 rounded-[10px] border border-slate-200 justify-start items-center inline-flex">
-              <div className="Expand grow shrink basis-0 self-stretch px-10 justify-center items-center gap-2 flex">
-                <div className="ExpandList20 text-brand-tq text-[10px] sm:text-xs font-normal font-['Inter']">
-                  Expand List {list.items.length}
-                </div>
-                <Icon
-                  sizeRules="w-3 h-3 sm:w-4 sm:h-4"
-                  fill="stroke-2 stroke-brand-tq fill-none"
-                >
-                  <ExpandDownIcon />
-                </Icon>
-                <div className="Icon w-2.5 h-2.5 py-[2.50px] justify-center items-center flex" />
-              </div>
-            </div>
-          )}
+          {showItems &&
+            items &&
+            items.map((item, index) => (
+              <Draggable
+                key={item.order}
+                onStop={(e, data) => {
+                  const newPosition = index + Math.round(data.y / itemHeight); // Assuming each item's height is 50px
+                  handleStop(index, newPosition);
+                }}
+                onStart={() => onDragStart(index)}
+                onDrag={(e, data) => {
+                  const newPosition = index + Math.round(data.y / itemHeight); // Assuming each item's height is 50px
+                  setInsertIndex(newPosition);
+                }}
+                axis="y"
+                position={{ x: 0, y: item.order - 1 }}
+                // grid={[1, 70]}
+              >
+                <ListItem
+                  insertVisible={insertIndex == index && !item.selected}
+                  fromTop={draggedIndex < index}
+                  order={index + 1}
+                  searchRef={searchInputRef}
+                  onDelete={onDelete}
+                  dragging={dragging}
+                  selected={item.selected}
+                  show={item.show}
+                  setShowToItem={setShowToItem}
+                />
+              </Draggable>
+            ))}
         </div>
-      )}
-      <div className="text-light-text font-light mx-2 smallest-text">
+        {list.items.length > 0 && (
+          <div
+            className="cursor-pointer self-stretch px-0 sm:px-1 md:px-2.5 flex-col justify-start items-start gap-1 sm:gap-1.5 flex"
+            onClick={toggleShowItems}
+          >
+            {showItems ? (
+              <div className="opacity-50 hover:opacity-100 w-full h-4 sm:h-6 bg-theme-base bg-opacity-70 rounded-b-xl rounded-t-lg border border-slate-200 justify-start items-center inline-flex">
+                <div className="Expand grow shrink basis-0 self-stretch justify-center items-center flex">
+                  <Icon
+                    sizeRules="w-2 h-2 sm:w-3 sm:h-3"
+                    fill="rotate-180 stroke-2 stroke-brand-tq fill-none"
+                  >
+                    <ExpandDownIcon />
+                  </Icon>
+                </div>
+              </div>
+            ) : (
+              <div className="ShowDetailCard w-full h-4 sm:h-6 bg-white bg-opacity-20 rounded-[10px] border border-slate-200 justify-start items-center inline-flex">
+                <div className="Expand grow shrink basis-0 self-stretch px-10 justify-center items-center gap-2 flex">
+                  <div className="text-brand-tq text-[8px] sm:text-[10px] font-normal font-['Inter']">
+                    Expand List {list.items.length}
+                  </div>
+                  <Icon
+                    sizeRules="w-2 h-2 sm:w-3 sm:h-3"
+                    fill="stroke-2 stroke-brand-tq fill-none"
+                  >
+                    <ExpandDownIcon />
+                  </Icon>
+                  <div className="Icon w-2.5 h-2.5 py-[2.50px] justify-center items-center flex" />
+                </div>
+              </div>
+            )}
+          </div>
+        )}
+      </div>
+      <div className="text-light-text font-light mx-3 mt-1 smallest-text">
         {list.updatedAt &&
           list.items.length > 0 &&
           `Saved ${formatDateTime(list.updatedAt)}`}
--- client/src/pages/MyLists/ListOptionButton.jsx
+++ client/src/pages/MyLists/ListOptionButton.jsx
@@ -1,18 +1,18 @@
-import Icon from "../Common/Icon";
+import Icon from '../Common/Icon';
 
 const ListOptionButton = ({ children, active, onClick }) => {
   return (
     <button
       onClick={onClick}
       className={`${
-        active ? "bg-brand-tq" : "bg-theme-base"
-      } p-2 hover:bg-opacity-90 bg-opacity-60 rounded-full border flex-col justify-center items-center gap-2.5 inline-flex`}
+        active ? 'bg-brand-tq' : 'bg-theme-base'
+      } p-1.5 sm:p-2 hover:bg-opacity-90 bg-opacity-60 rounded-full border flex-col justify-center items-center gap-2.5 inline-flex`}
     >
       <Icon
-        fill={!active && "fill-transparent"}
+        fill={!active && 'fill-transparent'}
         sizeRules={`${
-          active ? "text-theme-base fill-theme-base" : "text-text-dark"
-        } w-2.5 h-2.5 sm:w-3.5 sm:h-3.5 group-hover:text-theme-base group-hover:fill-theme-base group-hover:stroke-2 stroke-1 sm:stroke-1.5 w-full h-full`}
+          active ? 'text-theme-base fill-theme-base' : 'text-text-dark'
+        } w-3 h-3 sm:w-3.5 sm:h-3.5 group-hover:text-theme-base group-hover:fill-theme-base group-hover:stroke-2 stroke-1 sm:stroke-1.5`}
       >
         {children}
       </Icon>
