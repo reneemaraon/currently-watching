f928b97f164aa9ffd56b741d9d9a7d73a1a7cc70
Renee Maraon <reneemaraon@gmail.com>
2024-06-01 00:31:21 +0800

    cookie settings

--- api/index.js
+++ api/index.js
@@ -1,39 +1,39 @@
-require('dotenv').config(); // to access environment variables
-require('express-async-errors');
-const { ApolloServer } = require('@apollo/server');
-const gql = require('graphql-tag');
-const { buildSubgraphSchema } = require('@apollo/subgraph');
-const { expressMiddleware } = require('@apollo/server/express4');
-const resolvers = require('./resolvers');
-const { readFileSync } = require('fs');
+require("dotenv").config(); // to access environment variables
+require("express-async-errors");
+const { ApolloServer } = require("@apollo/server");
+const gql = require("graphql-tag");
+const { buildSubgraphSchema } = require("@apollo/subgraph");
+const { expressMiddleware } = require("@apollo/server/express4");
+const resolvers = require("./resolvers");
+const { readFileSync } = require("fs");
 
-const express = require('express');
-const MongoStore = require('connect-mongo');
+const express = require("express");
+const MongoStore = require("connect-mongo");
 const app = express();
 
-const passport = require('passport');
-const passportSetup = require('./services/passport');
-const session = require('express-session');
-const cors = require('cors');
+const passport = require("passport");
+const passportSetup = require("./services/passport");
+const session = require("express-session");
+const cors = require("cors");
 
 app.use(express.json());
 // app.set("trust proxy", 1);
 
 const typeDefs = gql(
-  readFileSync('schema.graphql', {
-    encoding: 'utf-8',
+  readFileSync("schema.graphql", {
+    encoding: "utf-8",
   })
 );
 
 app.use(
   session({
-    secret: 'secretcode',
+    secret: process.env.SECRET_KEY,
     resave: true,
     saveUninitialized: true,
     store: MongoStore.create({ mongoUrl: process.env.MONGO_URI }),
     cookie: {
-      sameSite: 'Lax',
-      secure: false,
+      sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax", // Use 'None' for cross-origin in production
+      secure: process.env.NODE_ENV === "production",
       maxAge: 1000 * 60 * 60 * 24 * 7, // One Week
     },
   })
@@ -42,7 +42,7 @@ app.use(
 app.use(
   cors({
     origin: process.env.CLIENT_HOME_PAGE_URL, // allow to server to accept request from different origin
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true, // allow session cookie from browser to pass through
   })
 );
@@ -50,19 +50,19 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-const connectDB = require('./db/connect');
+const connectDB = require("./db/connect");
 
 //routers
-const authRouter = require('./routes/auth');
-const showRouter = require('./routes/show');
-const reviewRouter = require('./routes/review');
+const authRouter = require("./routes/auth");
+const showRouter = require("./routes/show");
+const reviewRouter = require("./routes/review");
 
 //custom middlewares
-const errorHandlerMiddleware = require('./middlewares/errorHandler');
+const errorHandlerMiddleware = require("./middlewares/errorHandler");
 
-app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/shows', showRouter);
-app.use('/api/v1/reviews', reviewRouter);
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/shows", showRouter);
+app.use("/api/v1/reviews", reviewRouter);
 
 // Note you must call `start()` on the `ApolloServer`
 // instance before passing the instance to `expressMiddleware`
@@ -77,7 +77,7 @@ const server = new ApolloServer({
 const apollo_start = async () => {
   await server.start();
   app.use(
-    '/graphql',
+    "/graphql",
     expressMiddleware(server, {
       context: async ({ req }) => ({ user: req.user }),
     })
@@ -93,7 +93,7 @@ const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
     app.listen(port, () => {
-      console.log('Successfully connected to DB.');
+      console.log("Successfully connected to DB.");
       console.log(`Server is listening in ${port}`);
     });
   } catch (error) {
