463ef65b13cc097e77dc035014e4f4a2fb3b3c51
Renee Maraon <reneemaraon@gmail.com>
2024-06-01 13:49:57 +0800

    revert to lax

--- api/index.js
+++ api/index.js
@@ -32,12 +32,8 @@ app.use(
     saveUninitialized: false,
     store: MongoStore.create({ mongoUrl: process.env.MONGO_URI }),
     cookie: {
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-      path: "/",
-      domain:
-        process.env.NODE_ENV === "production" ? ".currently-active.live" : "",
-      httpOnly: false,
+      secure: false,
+      sameSite: "lax",
       maxAge: 1000 * 60 * 60 * 24 * 7, // One Week
     },
   })
