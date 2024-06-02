851e56b2381c887f87729a40074392acdad9c17b
Renee Maraon <reneemaraon@gmail.com>
2024-06-02 23:29:14 +0800

    Revert "fix"
    This reverts commit 23ad47656e9a094849483cdd72ddbb1188a7f5c0.

--- api/index.js
+++ api/index.js
@@ -1,43 +1,41 @@
-require('dotenv').config(); // to access environment variables
-require('express-async-errors');
-const { ApolloServer } = require('@apollo/server');
-const gql = require('graphql-tag');
-const { buildSubgraphSchema } = require('@apollo/subgraph');
-const { expressMiddleware } = require('@apollo/server/express4');
-const resolvers = require('./resolvers');
-const { readFileSync } = require('fs');
+require("dotenv").config(); // to access environment variables
+require("express-async-errors");
+const { ApolloServer } = require("@apollo/server");
+const gql = require("graphql-tag");
+const { buildSubgraphSchema } = require("@apollo/subgraph");
+const { expressMiddleware } = require("@apollo/server/express4");
+const resolvers = require("./resolvers");
+const { readFileSync } = require("fs");
 
-const express = require('express');
-const MongoStore = require('connect-mongo');
+const express = require("express");
+const MongoStore = require("connect-mongo");
 const app = express();
 
-const passport = require('passport');
-const passportSetup = require('./services/passport');
-const session = require('express-session');
-const cors = require('cors');
+const passport = require("passport");
+const passportSetup = require("./services/passport");
+const session = require("express-session");
+const cors = require("cors");
 
 app.use(express.json());
-app.set('trust proxy', 1);
+app.set("trust proxy", 1);
 
 const typeDefs = gql(
-  readFileSync('schema.graphql', {
-    encoding: 'utf-8',
+  readFileSync("schema.graphql", {
+    encoding: "utf-8",
   })
 );
 
 app.use(
   session({
     secret: process.env.SECRET_KEY,
-    resave: true,
-    saveUninitialized: true,
+    resave: false,
+    saveUninitialized: false,
     store: MongoStore.create({ mongoUrl: process.env.MONGO_URI }),
     cookie: {
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax',
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       domain:
-        process.env.NODE_ENV === 'production'
-          ? '.currently-watching.live'
-          : null,
+        process.env.NODE_ENV === "production" ? ".currently-watching.live" : "",
       maxAge: 1000 * 60 * 60 * 24 * 7, // One Week
     },
   })
@@ -46,7 +44,7 @@ app.use(
 app.use(
   cors({
     origin: process.env.CLIENT_HOME_PAGE_URL, // allow to server to accept request from different origin
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true, // allow session cookie from browser to pass through
   })
 );
@@ -54,19 +52,19 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-const connectDB = require('./db/connect');
+const connectDB = require("./db/connect");
 
 //routers
-const authRouter = require('./routes/auth');
-const showRouter = require('./routes/show');
-const reviewRouter = require('./routes/review');
+const authRouter = require("./routes/auth");
+const showRouter = require("./routes/show");
+const reviewRouter = require("./routes/review");
 
 //custom middlewares
-const errorHandlerMiddleware = require('./middlewares/errorHandler');
+const errorHandlerMiddleware = require("./middlewares/errorHandler");
 
-app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/shows', showRouter);
-app.use('/api/v1/reviews', reviewRouter);
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/shows", showRouter);
+app.use("/api/v1/reviews", reviewRouter);
 
 // Note you must call `start()` on the `ApolloServer`
 // instance before passing the instance to `expressMiddleware`
@@ -81,7 +79,7 @@ const server = new ApolloServer({
 const apollo_start = async () => {
   await server.start();
   app.use(
-    '/graphql',
+    "/graphql",
     expressMiddleware(server, {
       context: async ({ req }) => ({ user: req.user }),
     })
@@ -97,7 +95,7 @@ const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
     app.listen(port, () => {
-      console.log('Successfully connected to DB.');
+      console.log("Successfully connected to DB.");
       console.log(`Server is listening in ${port}`);
     });
   } catch (error) {
