368e0a37fd9997a943537efea3f8c05574f4c174
Renee Maraon <reneemaraon@gmail.com>
2024-06-02 23:29:32 +0800

    Revert "fix none call"
    This reverts commit 56a83ec14563e6c30e548cab67c835143e46d6dc.

--- api/index.js
+++ api/index.js
@@ -33,7 +33,7 @@ app.use(
     store: MongoStore.create({ mongoUrl: process.env.MONGO_URI }),
     cookie: {
       secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      sameSite: process.env.NODE_ENV === "production" ? " none" : "lax",
       // domain:
       //   process.env.NODE_ENV === "production" ? "currently-watching.live" : "",
       maxAge: 1000 * 60 * 60 * 24 * 7, // One Week
