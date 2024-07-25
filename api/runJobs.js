3636b19e09a75c1faaee44ace03b3cdb400c47f2
Francesca Renee Maraon <reneemaraon@gmail.com>
2024-07-25 21:14:17 +0800

    show card changes

--- api/runJobs.js
+++ api/runJobs.js
@@ -1,13 +1,13 @@
 // Import necessary libraries
-require("dotenv").config();
-const mongoose = require("mongoose");
-const recalculateRatings = require("./cronjobs/showUpdateRatings");
-const updateReviews = require("./cronjobs/updateReviews");
-const removeDuplicates = require("./cronjobs/removeDuplicates");
-const populateDb = require("./cronjobs/populateDb");
-const connectDB = require("./db/connect");
-const updateShows = require("./cronjobs/updateShows");
-const deleteNonKor = require("./cronjobs/deleteNonKr");
+require('dotenv').config();
+const mongoose = require('mongoose');
+const recalculateRatings = require('./cronjobs/showUpdateRatings');
+const updateReviews = require('./cronjobs/updateReviews');
+const removeDuplicates = require('./cronjobs/removeDuplicates');
+const populateDb = require('./cronjobs/populateDb');
+const connectDB = require('./db/connect');
+const updateShows = require('./cronjobs/updateShows');
+const deleteNonKor = require('./cronjobs/deleteNonKr');
 
 // Logic to perform the job
 async function runJobs() {
@@ -15,19 +15,19 @@ async function runJobs() {
     // Call your recalculateRatings function or any other logic here
     await connectDB(process.env.MONGO_URI);
 
+    await updateShows();
+    await populateDb();
+    await updateReviews();
+    await removeDuplicates();
     await recalculateRatings();
-    // await removeDuplicates();
-    // await populateDb();
     // console.log('Recalculated show items  successfully.');
 
-    // await updateShows();
     // await deleteNonKor();
-    // await updateReviews();
     // console.log('Updated review like and comments counts successfully.');
 
     // console.log('Jobs completed successfully.');
   } catch (error) {
-    console.error("Error running jobs:", error);
+    console.error('Error running jobs:', error);
   } finally {
     // Close the database connection
     mongoose.disconnect();
